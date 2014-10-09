442886bcc9026ccd0e5a28f6e7504c4f13ca3842
Raul Lorenzo <raul.lorenzo.67@gmail.com>
2014-10-09 18:16:50 +0200

    Ej 5 clases esenciales

---
+++ src/main/java/edu/upc/eetac/dsa/rlorenzo/Ejercicio5/Archivo.java
@@ -0,0 +1,35 @@
+package edu.upc.eetac.dsa.rlorenzo.Ejercicio5;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Archivo {
+	
+	public int cuentaBytes(){
+		int num=0;
+		
+		try {
+			FileReader fr = new FileReader("/home/raul/imagen.jpg");
+			BufferedReader br = new BufferedReader(fr);
+			int aux = br.read();
+			int count =-1;
+			while(aux!=-1){
+				aux = br.read();
+				count++;
+			}
+			num = count;
+			br.close();
+			fr.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return num;
+	}
+	
+	
+
+}
---
+++ src/main/java/edu/upc/eetac/dsa/rlorenzo/Ejercicio5/Main.java
@@ -0,0 +1,11 @@
+package edu.upc.eetac.dsa.rlorenzo.Ejercicio5;
+
+
+
+public class Main {
+	public static void main(String[] args){
+		Archivo archivo = new Archivo();
+		System.out.println("El archivo imagen.jpg tiene un peso de: "+archivo.cuentaBytes()+"Bytes");
+	}
+
+}
